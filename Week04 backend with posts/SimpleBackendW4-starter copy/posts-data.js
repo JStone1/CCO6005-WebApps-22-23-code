ae36c472fe71e2939860487c1eaa3b74dfc66632
JStone1 <john.stone111@icloud.com>
2023-03-15 14:50:02 +0000

    Code updated from class

---
+++ Week04 backend with posts/SimpleBackendW4-starter copy/posts-data.js
@@ -0,0 +1,13 @@
+const posts = [];
+
+function addNewPost(userID, message) {
+  let myPost = {
+    postedBy: userID,
+    message: message,
+    likes: 0,
+    time: Date.now(),
+  };
+  posts.unshift(myPost);
+}
+
+module.exports = { addNewPost };
